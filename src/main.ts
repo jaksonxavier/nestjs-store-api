eb43a66c39e5ceedbc46c9be22fd95657aa923e0
Jakson Xavier <jackd.xavier@gmail.com>
2021-07-05 09:13:20 -0300

    feat: grpc crud products

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { NotFoundExceptionFilter } from './exception-filters/not-found.exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalFilters(new NotFoundExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      url: '0.0.0.0:50051',
+      package: 'product',
+      protoPath: join(__dirname, 'products', 'proto', 'product.proto'),
+    },
+  });
+
+  await app.startAllMicroservicesAsync();
+
   await app.listen(3000);
 }
 bootstrap();
